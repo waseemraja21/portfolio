0ded1446141c8333efb4e3ff36516111d8aab9f0
waseemraja21 <waseemrajaanim@gmail.com>
2024-08-08 17:12:54 +0530

    contact updated

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="bg-custom-dblue">
+    <div className="bg-custom-dblue h-1/2 ">
       <h2 className="text-4xl text-center font-poppins font-semibold py-10">
         CONTACT
       </h2>
       <div className="flex flex-row items-center py-10 justify-center">
-        <div className="px-20">
+        <div className="px-4 md:px-20">
           <h3 className="font-thin text-2xl">Let's Connect</h3>
           <h4 className="font-semibold text-2xl">DIGITALLY</h4>
         </div>
-        <div className="px-20 text-2xl font-thin">
+        <div className="px-4 md:px-20 text-2xl font-thin">
           <h3>
             <a
               href="mailto:waseemrajaanim@gmail.com"
